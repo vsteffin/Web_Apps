78f4b0288016af36374af3552661c193756f745c
Steffin Varghese <49582772+vsteffin@users.noreply.github.com>
2025-03-24 23:51:23 +0530

    Create config.js

---
+++ Exam_Prep_2/config.js
@@ -0,0 +1,10 @@
+// This file will be populated with your Firebase credentials by Netlify
+window.firebaseConfig = {
+apiKey: "FIREBASE_API_KEY",
+authDomain: "FIREBASE_AUTH_DOMAIN",
+databaseURL: "FIREBASE_DATABASE_URL",
+projectId: "FIREBASE_PROJECT_ID", 
+storageBucket: "FIREBASE_STORAGE_BUCKET",
+messagingSenderId: "FIREBASE_MESSAGING_SENDER_ID",
+appId: "FIREBASE_APP_ID"
+};
